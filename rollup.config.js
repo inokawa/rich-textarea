4bf9ff54d11108db6a9b62d7629dc8ea4f2ecd42
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-27 22:23:02 +0900

    Init project

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+  plugins: [typescript()],
+};
