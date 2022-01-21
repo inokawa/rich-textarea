f5bfff0c7daab42c6053d6c00f553622d908ba48
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-22 04:21:32 +0900

    Fix logic

--- src/index.ts
+++ src/index.ts
@@ -87,55 +87,78 @@ const execReg = (reg: RegExp, text: string): RegExpExecArray[] => {
   return results;
 };
 
-const tokenize = (value: string, matchers: Matcher[]): React.ReactNode => {
-  const styles: { [key: number]: React.CSSProperties } = {};
-  const matches: { s: number; e: number; i: number[] }[] = [];
+type MatchChunk = [start: number, end: number, matcherIndex: number];
+
+export const merge = (
+  ranges: MatchChunk[]
+): [number, number, { [key: number]: true | undefined }][] => {
+  ranges.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+
+  const results: [number, number, { [key in number]: true | undefined }][] = [];
+  let last: [number, number, { [key in number]: true | undefined }];
+
+  ranges.forEach(([start, end, styleIndex]) => {
+    if (!last || start >= last[1]) {
+      results.push((last = [start, end, { [styleIndex]: true }]));
+    } else if (end > last[1]) {
+      last[2][styleIndex] = true;
+      results.push((last = [last[1], end, { [styleIndex]: true }]));
+    } else if (end <= last[1]) {
+      const lastEnd = last[1];
+      const lastStyle = { ...last[2] };
+      const lastStyle2 = { ...last[2] };
+      if (last[0] !== start) {
+        last[1] = start;
+      } else {
+        results.pop();
+      }
+      lastStyle[styleIndex] = true;
+      results.push((last = [start, end, lastStyle]));
+      if (end !== lastEnd) {
+        results.push((last = [end, lastEnd, lastStyle2]));
+      }
+    }
+  });
+
+  return results;
+};
+
+const valueToReact = (value: string, matchers: Matcher[]): React.ReactNode => {
+  const styles: { [key: string]: React.CSSProperties } = {};
+  const matches: MatchChunk[] = [];
   matchers.forEach(([matcher, style], i) => {
     matches.push(
-      ...execReg(matcher, value).map((m) => {
+      ...execReg(matcher, value).map((m): MatchChunk => {
         const start = m.index;
         const end = m.index + m[0].length;
-        return { s: start, e: end, i: [i] };
+        return [start, end, i];
       })
     );
     styles[String(i)] = style;
   });
-  matches.sort((a, b) => {
-    const sd = a.s - b.s;
-    if (sd === 0) {
-      const ed = a.e - b.e;
-      if (ed === 0) {
-        return 0;
-      } else if (ed < 0) {
-        return -1;
-      } else {
-        return 1;
-      }
-    } else if (sd < 0) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
 
+
+  const chunks = merge(matches);
   const res: (string | React.ReactElement)[] = [];
   let prevEnd = 0;
   let prevStart = 0;
-  for (let i = 0; i < matches.length; i++) {
-    const { s: start, e: end, i: styleIndexes } = matches[i];
+  for (let i = 0; i < chunks.length; i++) {
+    const [start, end, styleIndexes] = chunks[i];
     res.push(value.slice(prevEnd, start));
+
+    const style = Object.keys(styleIndexes).reduce((acc, i) => {
+      const s = styles[i];
+      Object.keys(s).forEach((k) => {
+        (acc as any)[k] = (s as any)[k];
+      });
+      return acc;
+    }, {} as React.CSSProperties);
     res.push(
       createElement(
         "span",
         {
           key: i,
-          style: styleIndexes.reduce((acc, i) => {
-            const s = styles[i];
-            Object.keys(s).forEach((k) => {
-              (acc as any)[k] = (s as any)[k];
-            });
-            return acc;
-          }, {} as React.CSSProperties),
+          style,
         },
         value.slice(start, end)
       )
--- stories/basics.stories.tsx
+++ stories/basics.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react";
-import { Textarea } from "../src";
+import { Matcher, Textarea } from "../src";
 
 const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -43,7 +43,7 @@ export default {
 
 const style = { width: "600px", height: "400px" };
 
-const basicMatcher = [
+const basicMatcher: Matcher[] = [
   [/[A-Z][a-z]+/g, { borderRadius: "3px", backgroundColor: "#d0bfff" }],
 ];
 
@@ -59,7 +59,8 @@ export const Basic = () => {
   );
 };
 
-const manyMatcher = [
+const manyMatcher: Matcher[] = [
+  [/[A-Z][a-z]+/g, { borderRadius: "3px", backgroundColor: "#d0bfff" }],
   [/ipsum/g, { color: "blue", textDecoration: "underline wavy" }],
   [/[abcdeip]/g, { color: "red", fontWeight: "bold" }],
 ];
@@ -76,7 +77,7 @@ export const Many = () => {
   );
 };
 
-const emojiMatcher = [
+const emojiMatcher: Matcher[] = [
   [
     /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu,
     { background: "yellow" },
