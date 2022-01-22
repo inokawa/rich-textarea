175a14f8e9de847c2a3f7d880f0fbab367b385a0
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-23 00:00:11 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import React, {
   useCallback,
   createElement,
 } from "react";
+import { execReg, MatchChunk, merge } from "./utils";
 
 const STYLE_KEYS: (keyof React.CSSProperties)[] = [
   "padding",
@@ -77,52 +78,6 @@ const getHorizontalPadding = (style: CSSStyleDeclaration): number => {
   );
 };
 
-const execReg = (reg: RegExp, text: string): RegExpExecArray[] => {
-  const results: RegExpExecArray[] = [];
-  let match = reg.exec(text);
-  while (!!match) {
-    results.push(match);
-    match = reg.exec(text);
-  }
-  return results;
-};
-
-type MatchChunk = [start: number, end: number, matcherIndex: number];
-
-export const merge = (
-  ranges: MatchChunk[]
-): [number, number, { [key: number]: true | undefined }][] => {
-  ranges.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
-
-  const results: [number, number, { [key in number]: true | undefined }][] = [];
-  let last: [number, number, { [key in number]: true | undefined }];
-
-  ranges.forEach(([start, end, styleIndex]) => {
-    if (!last || start >= last[1]) {
-      results.push((last = [start, end, { [styleIndex]: true }]));
-    } else if (end > last[1]) {
-      last[2][styleIndex] = true;
-      results.push((last = [last[1], end, { [styleIndex]: true }]));
-    } else if (end <= last[1]) {
-      const lastEnd = last[1];
-      const lastStyle = { ...last[2] };
-      const lastStyle2 = { ...last[2] };
-      if (last[0] !== start) {
-        last[1] = start;
-      } else {
-        results.pop();
-      }
-      lastStyle[styleIndex] = true;
-      results.push((last = [start, end, lastStyle]));
-      if (end !== lastEnd) {
-        results.push((last = [end, lastEnd, lastStyle2]));
-      }
-    }
-  });
-
-  return results;
-};
-
 const valueToReact = (value: string, matchers: Matcher[]): React.ReactNode => {
   const styles: { [key: string]: React.CSSProperties } = {};
   const matches: MatchChunk[] = [];
@@ -136,7 +91,6 @@ const valueToReact = (value: string, matchers: Matcher[]): React.ReactNode => {
     );
     styles[String(i)] = style;
   });
-
 
   const chunks = merge(matches);
   const res: (string | React.ReactElement)[] = [];
