ab1c45586f1278563477f4c7c753a74f15a2c816
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-30 20:21:50 +0900

    Add toobar example

---
+++ stories/toolbar.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useMemo, useRef, useState } from "react";
+import { RichTextarea } from "../src";
+import { RichTextareaHandle } from "../src";
+
+export default {
+  title: "examples",
+};
+
+const style = { width: "400px", height: "300px" };
+
+export const Toolbar = () => {
+  const ref = useRef<RichTextareaHandle>(null);
+  const [text, setText] = useState(`Select text and click any button.\n\n`);
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            const start = ref.current.textareaRef.current.selectionStart;
+            const end = ref.current.textareaRef.current.selectionEnd;
+            ref.current.setRangeText(
+              `**${text.slice(start, end)}**`,
+              start,
+              end
+            );
+          }}
+        >
+          <b>B</b>
+        </button>
+        <button
+          onClick={() => {
+            const start = ref.current.textareaRef.current.selectionStart;
+            const end = ref.current.textareaRef.current.selectionEnd;
+            ref.current.setRangeText(`*${text.slice(start, end)}*`, start, end);
+          }}
+        >
+          <i>I</i>
+        </button>
+        <button
+          onClick={() => {
+            const start = ref.current.textareaRef.current.selectionStart;
+            const end = ref.current.textareaRef.current.selectionEnd;
+            ref.current.setRangeText(
+              `~~${text.slice(start, end)}~~`,
+              start,
+              end
+            );
+          }}
+        >
+          <s>S</s>
+        </button>
+      </div>
+      <RichTextarea
+        ref={ref}
+        style={style}
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+    </div>
+  );
+};
