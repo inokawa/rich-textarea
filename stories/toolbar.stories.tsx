5241e3b7603c0103e6c06066287c833b4bf9d367
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-31 01:14:10 +0900

    Update toolbar.stories.tsx

--- stories/toolbar.stories.tsx
+++ stories/toolbar.stories.tsx
@@ -12,8 +12,8 @@ const style = { width: "400px", height: "300px" };
 export const Toolbar = () => {
   const ref = useRef<RichTextareaHandle>(null);
   const [text, setText] = useState(`Select text and click any button.\n\n`);
-  const [[selectionStart, selectionEnd], setSelection] = useState<
-    [number, number] | []
+  const [[selectionStart, selectionEnd, pos], setSelection] = useState<
+    [number, number, { top: number; left: number }] | []
   >([]);
   const disabled =
     selectionStart == null ||
@@ -21,91 +21,84 @@ export const Toolbar = () => {
     selectionStart === selectionEnd;
 
   return (
-    <div>
-      <div>
-        <button
-          disabled={disabled}
-          onClick={() => {
-            const start = ref.current.selectionStart;
-            const end = ref.current.selectionEnd;
-            ref.current.setRangeText(
-              `**${text.slice(start, end)}**`,
-              start,
-              end
-            );
-          }}
-        >
-          <b>B</b>
-        </button>
-        <button
-          disabled={disabled}
-          onClick={() => {
-            const start = ref.current.selectionStart;
-            const end = ref.current.selectionEnd;
-            ref.current.setRangeText(`*${text.slice(start, end)}*`, start, end);
-          }}
-        >
-          <i>I</i>
-        </button>
-        <button
-          disabled={disabled}
-          onClick={() => {
-            const start = ref.current.selectionStart;
-            const end = ref.current.selectionEnd;
-            ref.current.setRangeText(
-              `~~${text.slice(start, end)}~~`,
-              start,
-              end
-            );
-          }}
-        >
-          <s>S</s>
-        </button>
-      </div>
+    <div style={{ position: "relative", paddingTop: 16 }}>
       <RichTextarea
         ref={ref}
+        value={text}
         style={style}
         onChange={(e) => setText(e.target.value)}
-        onSelectionChange={(p) => {
-          setSelection([p.selectionStart, p.selectionEnd]);
+        onBlur={() => {
+          ref.current.setSelectionRange(0, 0);
+          ref.current.blur();
         }}
-        value={text}
+        onSelectionChange={(p) => {
+          setSelection([
+            p.selectionStart,
+            p.selectionEnd,
+            p.focused
+              ? {
+                  top: p.top - p.height * 1.5 /* FIXME */,
+                  left: p.left,
+                }
+              : undefined,
+          ]);
+        }}
       />
-      {/* {pos &&
+      {pos &&
+        !disabled &&
         createPortal(
           <div
             style={{
               position: "fixed",
               top: pos.top,
               left: pos.left,
-              fontSize: "16px",
-              border: "solid 1px gray",
-              borderRadius: "3px",
-              background: "white",
-              cursor: "pointer",
             }}
           >
-            {chars.map((c, i) => (
-              <div
-                key={c.key}
-                style={{
-                  padding: "4px",
-                  ...(index === i && {
-                    color: "white",
-                    background: "#2A6AD3",
-                  }),
-                }}
-                onMouseDown={(e) => {
-                  e.preventDefault();
-                  complete(i);
-                }}
-              >
-                {`${c.emoji} ${c.key}`}
-              </div>
-            ))}
+            <button
+              disabled={disabled}
+              onMouseDown={() => {
+                const start = ref.current.selectionStart;
+                const end = ref.current.selectionEnd;
+                ref.current.setRangeText(
+                  `**${text.slice(start, end)}**`,
+                  start,
+                  end
+                );
+              }}
+            >
+              <b>B</b>
+            </button>
+            <button
+              disabled={disabled}
+              onMouseDown={() => {
+                const start = ref.current.selectionStart;
+                const end = ref.current.selectionEnd;
+                ref.current.setRangeText(
+                  `*${text.slice(start, end)}*`,
+                  start,
+                  end
+                );
+              }}
+            >
+              <i>I</i>
+            </button>
+            <button
+              disabled={disabled}
+              onMouseDown={() => {
+                const start = ref.current.selectionStart;
+                const end = ref.current.selectionEnd;
+                ref.current.setRangeText(
+                  `~~${text.slice(start, end)}~~`,
+                  start,
+                  end
+                );
+              }}
+            >
+              <s>S</s>
+            </button>
           </div>,
           document.body
-        )} */}
+        )}
     </div>
   );
 };
