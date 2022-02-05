811ae05b4eba80e04d09e63c55f2bbc2314fd5a2
inokawa <48897392+inokawa@users.noreply.github.com>
2022-02-05 10:51:58 +0900

    Create utils.spec.ts

---
+++ src/utils.spec.ts
@@ -0,0 +1,32 @@
+import { merge } from "./utils";
+
+describe("merge", () => {
+  it("ipsum", () => {
+    expect(
+      merge([
+        [0, 1, 1],
+        [0, 5, 2],
+        [1, 2, 3],
+      ])
+    ).toEqual([
+      [0, 1, { 1: true, 2: true }],
+      [1, 2, { 2: true, 3: true }],
+      [2, 5, { 2: true }],
+    ]);
+  });
+  it("ipsumi", () => {
+    expect(
+      merge([
+        [0, 1, 1],
+        [0, 5, 2],
+        [1, 2, 3],
+        [5, 6, 4],
+      ])
+    ).toEqual([
+      [0, 1, { 1: true, 2: true }],
+      [1, 2, { 2: true, 3: true }],
+      [2, 5, { 2: true }],
+      [5, 6, { 4: true }],
+    ]);
+  });
+});
