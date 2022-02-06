892b090afd50e67cdf608efb0992be363db8f034
inokawa <48897392+inokawa@users.noreply.github.com>
2022-02-07 02:31:24 +0900

    Update kuromoji.stories.tsx

--- stories/kuromoji.stories.tsx
+++ stories/kuromoji.stories.tsx
@@ -11,11 +11,50 @@ const style: React.CSSProperties = {
   height: "400px",
 };
 
+const Mark = ({
+  pos,
+  children,
+  onMouseMove,
+}: {
+  pos: string;
+  children: string;
+  onMouseMove: () => void;
+}) => {
+  const color =
+    pos === "名詞"
+      ? "rgba(255, 0, 0, 0.2)"
+      : pos === "動詞"
+      ? "rgba(0, 255, 0, 0.2)"
+      : pos === "形容詞"
+      ? "rgba(0, 0, 255, 0.2)"
+      : pos === "副詞"
+      ? "rgba(255, 0, 255, 0.2)"
+      : pos === "助詞"
+      ? "rgba(0, 255, 255, 0.2)"
+      : pos === "助動詞"
+      ? "rgba(255, 255, 0, 0.2)"
+      : undefined;
+
+  return (
+    <span
+      style={{
+        position: "relative",
+        background: color,
+        outline: "solid 1px lightgray",
+      }}
+      onMouseMove={onMouseMove}
+    >
+      {children}
+    </span>
+  );
+};
+
 export const Kuromoji = () => {
   const [text, setText] = useState(
     "すもももももももものうち。\n\n吾輩 （ わがはい ） は猫である。名前はまだ無い。\n\nあのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n\n山路を登りながら、こう考えた。智に働けば角が立つ。情に棹させば流される。意地を通せば窮屈だ。とかくに人の世は住みにくい。住みにくさが高じると、安い所へ引き越したくなる。どこへ越しても住みにくいと悟った時、詩が生れて、画が出来る。"
   );
   const [tokenizer, setTokenizer] = useState<Tokenizer | null>(null);
+  const [description, setDescription] = useState<string | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -29,6 +68,7 @@ export const Kuromoji = () => {
   return (
     <div>
       {!tokenizer && <div>{"Loading dictionaries"}</div>}
+      <div>{description}</div>
       <RichTextarea
         style={style}
         onChange={useCallback((e) => setText(e.target.value), [])}
@@ -38,31 +78,26 @@ export const Kuromoji = () => {
           if (!tokenizer) return v;
           const tokens = tokenizer.tokenize(text);
           const nodes: React.ReactElement[] = [];
-          for (const { pos, surface_form, word_position } of tokens) {
-            const color =
-              pos === "名詞"
-                ? "rgba(255, 0, 0, 0.2)"
-                : pos === "動詞"
-                ? "rgba(0, 255, 0, 0.2)"
-                : pos === "形容詞"
-                ? "rgba(0, 0, 255, 0.2)"
-                : pos === "副詞"
-                ? "rgba(255, 0, 255, 0.2)"
-                : pos === "助詞"
-                ? "rgba(0, 255, 255, 0.2)"
-                : pos === "助動詞"
-                ? "rgba(255, 255, 0, 0.2)"
-                : undefined;
+          for (const {
+            pos,
+            pos_detail_1,
+            pos_detail_2,
+            pos_detail_3,
+            surface_form,
+            word_position,
+          } of tokens) {
             nodes.push(
-              <span
+              <Mark
                 key={word_position}
-                style={{
-                  background: color,
-                  outline: "solid 1px lightgray",
-                }}
+                pos={pos}
+                onMouseMove={() =>
+                  setDescription(
+                    `${surface_form}: ${pos} | ${pos_detail_1} | ${pos_detail_2} | ${pos_detail_3}`
+                  )
+                }
               >
                 {surface_form}
-              </span>
+              </Mark>
             );
           }
           return nodes;
