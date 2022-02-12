8e44f7c3e65c9919e1a67cb1378c0f18c58e5f08
inokawa <48897392+inokawa@users.noreply.github.com>
2022-02-12 17:12:54 +0900

    Update tagging.stories.tsx

--- stories/tagging.stories.tsx
+++ stories/tagging.stories.tsx
@@ -6,7 +6,7 @@ export default {
 };
 
 const style: React.CSSProperties = {
-  width: "600px",
+  width: "400px",
   whiteSpace: "pre",
   overflowWrap: "normal",
   overflowX: "auto",
@@ -15,14 +15,20 @@ const style: React.CSSProperties = {
 
 const SPACER = " ";
 
-const Tag = ({ children }: { children: string }) => {
+const Tag = ({
+  children,
+  selected,
+}: {
+  children: string;
+  selected: boolean;
+}) => {
   return (
     <span
       style={{
-        background: "whitesmoke",
-        color: "slategray",
+        background: selected ? "lightgray" : "whitesmoke",
+        color: "dimgray",
         borderRadius: "4px",
-        outline: "solid 1px slategray",
+        outline: "solid 1px dimgray",
       }}
     >
       {children}
@@ -32,16 +38,30 @@ const Tag = ({ children }: { children: string }) => {
 
 export const Tagging = () => {
   const ref = useRef<RichTextareaHandle>(null);
-  const [tags, setTags] = useState<string[]>(["Apple", "Orange"]);
-  const [text, setText] = useState("Banana");
+  const [tags, setTags] = useState<string[]>(["Apple", "Orange", "Banana"]);
+  const [text, setText] = useState("");
   const [pos, setPos] = useState<number | null>(null);
 
-  const tagsText = tags.join(SPACER) + SPACER;
+  const tagsText = tags.join(SPACER) + (tags.length ? SPACER : "");
   const textStart = tagsText.length;
+  const tagIndex =
+    pos == null
+      ? -1
+      : tags.reduce(
+          (acc, t, i) => {
+            if (acc[0] !== -1) return acc;
+            const prev = acc[1];
+            acc[1] += t.length + SPACER.length;
+            if (prev <= pos && pos < acc[1]) {
+              acc[0] = i;
+            }
+            return acc;
+          },
+          [-1, 0] as [number, number]
+        )[0];
 
   return (
     <div>
-      <div>Type anything and press Enter key.</div>
       <RichTextarea
         ref={ref}
         rows={1}
@@ -61,7 +81,11 @@ export const Tagging = () => {
             case "Backspace":
               if (textStart < pos) return;
               e.preventDefault();
-              setTags((prev) => prev.slice(0, prev.length - 1));
+              setTags((prev) => {
+                const next = [...prev];
+                next.splice(tagIndex === -1 ? prev.length - 1 : tagIndex, 1);
+                return next;
+              });
               break;
             case "Delete":
               if (textStart < pos) return;
@@ -81,8 +105,19 @@ export const Tagging = () => {
       >
         {() => {
           return [
-            ...tags.flatMap((t, i) => [<Tag key={t}>{t}</Tag>, SPACER]),
-            text,
+            ...tags.flatMap((t, i) => [
+              <Tag key={t} selected={i == tagIndex}>
+                {t}
+              </Tag>,
+              SPACER,
+            ]),
+            text
+              ? text
+              : pos === textStart && (
+                  <span key={"last"} style={{ color: "darkgray" }}>
+                    Type anything and press Enter key...
+                  </span>
+                ),
           ];
         }}
       </RichTextarea>
