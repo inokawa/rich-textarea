8534d6444bcfe3c17057a4a7c11e0ce7a509bd19
inokawa <48897392+inokawa@users.noreply.github.com>
2022-02-17 02:15:33 +0900

    Create render.stories.tsx

---
+++ stories/render.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { RichTextarea } from "../src";
+
+export default {
+  title: "render",
+};
+
+const style = { width: "600px", height: "400px" };
+
+export const Basic = () => {
+  const [text, setText] = useState("Lorem ipsum");
+  return (
+    <RichTextarea
+      style={style}
+      onChange={(e) => setText(e.target.value)}
+      value={text}
+    >
+      {(v) => {
+        return v.split("").map((t, i) => (
+          <span key={i} style={{ color: i % 2 === 0 ? "red" : undefined }}>
+            {t}
+          </span>
+        ));
+      }}
+    </RichTextarea>
+  );
+};
