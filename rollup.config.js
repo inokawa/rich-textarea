5d0f0a880907833e650549bba14e7fc2626710a8
inokawa <48897392+inokawa@users.noreply.github.com>
2022-05-05 19:08:43 +0900

    Use @babel/plugin-transform-react-pure-annotations

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
@@ -21,8 +22,17 @@ export default {
       declaration: true,
       exclude: ["src/**/*.spec.*"],
     }),
+    getBabelOutputPlugin({
+      plugins: ["@babel/plugin-transform-react-pure-annotations"],
+    }),
     terser({
       compress: { passes: 3 },
+      format: {
+        // https://github.com/terser/terser/pull/550
+        // https://github.com/terser/terser/issues/968
+        comments: /@preserve|@lic|@cc_on|^\**!|__PURE__/i,
+        preserve_annotations: true,
+      },
     }),
   ],
   external: Object.keys(pkg.dependencies),
