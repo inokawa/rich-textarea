6b660661168fb6764a37d9a270b4eed4d312350a
inokawa <48897392+inokawa@users.noreply.github.com>
2022-09-06 12:52:03 +0900

    Add story with react-hook-form

--- stories/library.stories.tsx
+++ stories/library.stories.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import { useForm, Controller } from "react-hook-form";
 import { RichTextarea } from "../src";
 
 export default {
@@ -11,6 +12,14 @@ export const WithFormik = () => {
   const formik = useFormik({
     initialValues: { text: "Hello formik" },
     onSubmit: (data) => alert(JSON.stringify(data)),
+    validate(values) {
+      if (!values.text) {
+        return { text: "This field is required" };
+      }
+      if (!values.text.toLowerCase().includes("formik")) {
+        return { text: "Text must be include formik" };
+      }
+    },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
@@ -33,6 +42,65 @@ export const WithFormik = () => {
           ));
         }}
       </RichTextarea>
+      {formik.errors.text && (
+        <span style={{ color: "red" }}>{formik.errors.text}</span>
+      )}
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
+export const WithReactHookForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      text: "Hello react-hook-form",
+    },
+  });
+  const onSubmit = (data) => alert(JSON.stringify(data));
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        control={control}
+        name="text"
+        rules={{
+          required: true,
+          validate: (v) => v.toLowerCase().includes("hook"),
+        }}
+        render={({ field: { ref, onChange, onBlur, value } }) => (
+          <RichTextarea
+            ref={ref}
+            value={value}
+            style={style}
+            onChange={onChange}
+            onBlur={onBlur}
+          >
+            {(v) => {
+              return v.split("").map((t, i) => (
+                <span
+                  key={i}
+                  style={{
+                    color: "hook".includes(t.toLowerCase())
+                      ? "violet"
+                      : undefined,
+                  }}
+                >
+                  {t}
+                </span>
+              ));
+            }}
+          </RichTextarea>
+        )}
+      />
+      {errors.text?.type === "required" && (
+        <span style={{ color: "red" }}>This field is required</span>
+      )}
+      {errors.text?.type === "validate" && (
+        <span style={{ color: "red" }}>Text must be include hook</span>
+      )}
       <button type="submit">submit</button>
     </form>
   );
