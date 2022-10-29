f6e79f0fed18fb36c03092a0a466925fbafc96e5
inokawa <48897392+inokawa@users.noreply.github.com>
2022-10-29 20:54:51 +0900

    Use webpack v5

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,4 +9,23 @@ module.exports = {
     { from: "../node_modules/kuromoji/dict", to: "/dict" },
   ],
   framework: "@storybook/react",
+  core: {
+    builder: {
+      name: "webpack5",
+    },
+  },
+  features: {
+    // TODO: remove this after upgrading to storybook v7
+    storyStoreV7: true,
+  },
+  async webpackFinal(config, options) {
+    config.resolve.fallback = {
+      // For kuromoji
+      path: "path-browserify",
+      // For textlint
+      assert: "assert",
+      events: "events",
+    };
+    return config;
+  },
 };
