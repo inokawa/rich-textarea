6ece0911224fb697a9a32ef8462d2be408a88a82
inokawa <48897392+inokawa@users.noreply.github.com>
2022-11-30 01:44:26 +0900

    Upgrade to storybook v7 alpha

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,22 +3,30 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-docs"],
   staticDirs: [
     // for kuromojin
-    { from: "../node_modules/kuromoji/dict", to: "/dict" },
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: {
-      name: "webpack5",
+    {
+      from: "../node_modules/kuromoji/dict",
+      to: "/dict",
     },
-  },
-  features: {
-    // TODO: remove this after upgrading to storybook v7
-    storyStoreV7: true,
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
   async webpackFinal(config, options) {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
     config.resolve.fallback = {
       // For kuromoji
       path: "path-browserify",
