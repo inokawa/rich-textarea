2a5b45cbd822f280fabac45e6de1fc3ad662305b
inokawa <48897392+inokawa@users.noreply.github.com>
2023-02-21 11:48:42 +0900

    Add tests for react-dom/server

---
+++ src/textarea.ssr.spec.tsx
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment node
+ */
+import { it, describe, expect } from "@jest/globals";
+import { renderToString, renderToStaticMarkup } from "react-dom/server";
+import { RichTextarea } from ".";
+
+const NOP = () => {};
+
+describe("SSR", () => {
+  it("should succeed with renderToString", () => {
+    expect(
+      renderToString(
+        <RichTextarea
+          value={"Lorem ipsum dolor sit amet"}
+          style={{ width: "123px", height: "456px" }}
+          onChange={NOP}
+        >
+          {(v) => v}
+        </RichTextarea>
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("should succeed with renderToStaticMarkup", () => {
+    expect(
+      renderToStaticMarkup(
+        <RichTextarea
+          value={"Lorem ipsum dolor sit amet"}
+          style={{ width: "123px", height: "456px" }}
+          onChange={NOP}
+        >
+          {(v) => v}
+        </RichTextarea>
+      )
+    ).toMatchSnapshot();
+  });
+});
