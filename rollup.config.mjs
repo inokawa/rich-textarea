1ce886d5e60a1126475cc0bbcae87363c1d8986e
inokawa <48897392+inokawa@users.noreply.github.com>
2023-04-15 12:26:14 +0900

    Replace rollup-plugin-terser with @rollup/plugin-terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { getBabelOutputPlugin } from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 export default {
