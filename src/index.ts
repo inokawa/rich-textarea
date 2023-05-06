4f6ec84b6215e0a4e055b5ec5093add7c9e6e2c9
inokawa <48897392+inokawa@users.noreply.github.com>
2023-05-06 22:59:39 +0900

    Export RichInput as experimental_RichInput

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { RichTextarea } from "./textarea";
 export type { RichTextareaProps, RichTextareaHandle } from "./textarea";
-export { RichInput } from "./input";
+export { RichInput as experimental_RichInput } from "./input";
 export type { RichInputProps, RichInputHandle } from "./input";
 export * from "./renderers";
 export * from "./types";
