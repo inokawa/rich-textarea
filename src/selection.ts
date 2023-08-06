90a39c9a873c0f1497db21a5b8c8e0dea6a1a8fb
inokawa <48897392+inokawa@users.noreply.github.com>
2023-08-06 11:54:38 +0900

    Refactor

--- src/selection.ts
+++ src/selection.ts
@@ -6,21 +6,24 @@ export const initSelectionStore = (
   ref: React.RefObject<HTMLTextAreaElement | HTMLInputElement>,
   onSelectionUpdate: (range: SelectionRange) => void
 ) => {
-  let cache: [number | null, number | null] = [null, null];
+  let prevSelection: [number | null, number | null] = [null, null];
   let compositionEvent: CompositionEvent | void;
-  const _getSelection = (): [number | null, number | null] => {
+  const getSelection = (): [number | null, number | null] => {
     const selectionStart = handle._getSelectionStart();
     const selectionEnd = handle._getSelectionEnd();
-    if (cache[0] === selectionStart && cache[1] === selectionEnd) {
-      return cache;
+    if (
+      prevSelection[0] === selectionStart &&
+      prevSelection[1] === selectionEnd
+    ) {
+      return prevSelection;
     }
-    cache = [selectionStart, selectionEnd];
-    return cache;
+    prevSelection = [selectionStart, selectionEnd];
+    return prevSelection;
   };
   const handle = {
     _updateSeletion() {
       setTimeout(() => {
-        onSelectionUpdate(_getSelection());
+        onSelectionUpdate(getSelection());
       });
     },
     _setComposition(event: CompositionEvent | void) {
@@ -29,22 +32,18 @@ export const initSelectionStore = (
     _getSelectionStart(): number | null {
       const el = ref[refKey];
       if (!el) return null;
-      let pos = el.selectionStart!;
+      const pos = el.selectionStart!;
+      if (!compositionEvent) return pos;
       // compensate selection range during compositioning
-      if (compositionEvent) {
-        pos = Math.min(pos, el.selectionEnd! - compositionEvent.data.length);
-      }
-      return pos;
+      return Math.min(pos, el.selectionEnd! - compositionEvent.data.length);
     },
     _getSelectionEnd(): number | null {
       const el = ref[refKey];
       if (!el) return null;
-      let pos = el.selectionEnd!;
+      const pos = el.selectionEnd!;
       // compensate selection range during compositioning
-      if (compositionEvent) {
-        pos = Math.min(pos, el.selectionStart! + compositionEvent.data.length);
-      }
-      return pos;
+      if (!compositionEvent) return pos;
+      return Math.min(pos, el.selectionStart! + compositionEvent.data.length);
     },
   };
   return handle;
