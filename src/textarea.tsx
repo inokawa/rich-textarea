4a49dc320c0fdef2542fa80ded3dd3405cc8b715
inokawa <48897392+inokawa@users.noreply.github.com>
2023-10-05 12:05:02 +0900

    Add `-moz-text-size-adjust: 100%` style

--- src/textarea.tsx
+++ src/textarea.tsx
@@ -53,6 +53,7 @@ const Backdrop = memo(
             // https://stackoverflow.com/questions/2545542/font-size-rendering-inconsistencies-on-an-iphone
             textSizeAdjust: "100%",
             WebkitTextSizeAdjust: "100%",
+            MozTextSizeAdjust: "100%",
           }),
           [width]
         )}
