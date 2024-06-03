1dc0030f7aea582dfd61b0841826785663583f36
inokawa <48897392+inokawa@users.noreply.github.com>
2024-06-03 21:49:46 +0900

    Rename fn to shouldRender

--- src/renderers/regex/utils.ts
+++ src/renderers/regex/utils.ts
@@ -1,11 +1,15 @@
 /**
  * @internal
  */
-export const execReg = (reg: RegExp, text: string, fn?: (_: string) => boolean): RegExpExecArray[] => {
+export const execReg = (
+  reg: RegExp,
+  text: string,
+  shouldRender?: (matchedText: string) => boolean
+): RegExpExecArray[] => {
   const results: RegExpExecArray[] = [];
   let match: RegExpExecArray | null = null;
   while ((match = reg.exec(text))) {
-    if (fn && !fn(text)) {
+    if (shouldRender && !shouldRender(text)) {
       continue;
     }
     results.push(match);
