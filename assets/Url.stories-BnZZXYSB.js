d29157b6e6d87c236fd200cf1083885c1f85d213
inokawa <inokawa@users.noreply.github.com>
2024-11-04 14:22:59 +0000

    deploy: 4d993963274ea18653538c01c2a4e252543f1d46

--- assets/Url.stories-BnZZXYSB.js
+++ assets/Url.stories-BnZZXYSB.js
@@ -1,4 +1,4 @@
-import{j as s}from"./observer-Djkfms2H.js";import{r as x}from"./index-RYns6xqu.js";import{R as i}from"./textarea-zpBFEpjA.js";import{c as h}from"./index-xkcP6x86.js";const R={component:i},l={width:"400px",height:"300px"},p=h([[/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,({children:t,key:r,value:a})=>s.jsx("a",{href:a,target:"_blank",children:t},r)]]),e={render:()=>{const[t,r]=x.useState(`Click this url https://github.com/inokawa/rich-textarea !
+import{j as s}from"./observer-Djkfms2H.js";import{r as x}from"./index-RYns6xqu.js";import{R as i}from"./textarea-CUwguxoB.js";import{c as h}from"./index-xkcP6x86.js";const R={component:i},l={width:"400px",height:"300px"},p=h([[/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,({children:t,key:r,value:a})=>s.jsx("a",{href:a,target:"_blank",children:t},r)]]),e={render:()=>{const[t,r]=x.useState(`Click this url https://github.com/inokawa/rich-textarea !
 
 `);return s.jsx("div",{children:s.jsx(i,{style:l,onChange:a=>r(a.target.value),value:t,children:p})})}};var n,o,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
   render: () => {
