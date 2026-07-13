fa9855546831bef69b90e0181f26b4865895281c
inokawa <inokawa@users.noreply.github.com>
2026-07-13 01:11:46 +0000

    deploy: dba7cd07034f7dadd81d3c578b8aa9313847e2b9

---
+++ assets/Url.stories-ac35olfg.js
@@ -0,0 +1,12 @@
+import{n as e,o as t}from"./rolldown-runtime-DAXXjFlN.js";import{t as n}from"./react-BUD3sqOU.js";import{t as r}from"./jsx-runtime-CFwixLRt.js";import{r as i,s as a,t as o}from"./src-ByzE9CA6.js";var s,c,l,u,d,f,p;e((()=>{s=t(n()),o(),c=r(),l={component:a},u={width:`400px`,height:`300px`},d=i([[/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,({children:e,key:t,value:n})=>(0,c.jsx)(`a`,{href:n,target:`_blank`,children:e},t)]]),f={render:()=>{let[e,t]=(0,s.useState)(`Click this url https://github.com/inokawa/rich-textarea !
+
+`);return(0,c.jsx)(`div`,{children:(0,c.jsx)(a,{style:u,onChange:e=>t(e.target.value),value:e,children:d})})}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const [text, setText] = useState(\`Click this url https://github.com/inokawa/rich-textarea !\\n\\n\`);
+    return <div>
+        <RichTextarea style={style} onChange={e => setText(e.target.value)} value={text}>
+          {regexRenderer}
+        </RichTextarea>
+      </div>;
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Url`]}))();export{f as Url,p as __namedExportsOrder,l as default};
---
+++ assets/formatter-EIJCOSYU-CccHNRqL.js
@@ -0,0 +1,1 @@
+import{n as e}from"./rolldown-runtime-DAXXjFlN.js";import{a as t,i as n}from"./chunk-A242L54C-h7bV23nY.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-kRR8m_CV.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
